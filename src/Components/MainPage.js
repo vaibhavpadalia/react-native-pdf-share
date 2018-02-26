ef27530882f56c7ab5a50a338a23b6546e18c187
unknown <vaibhavpadalia07@gmail.com>
2018-02-26 18:17:21 +0530

    Works on Android too

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import { View, Text, WebView, Button, Share } from 'react-native';
-
+import React, { Component } from "react";
+import { View, Text, WebView, Button, Share, Platform } from "react-native";
 
 export default class MainPage extends Component {
-    share = () => {
-        Share.share({
-            message:"Something to be shared !!",
-            title: "Share"
-        })
-    }
+  share = () => {
+    Share.share({
+      title: "Share",
+      message: "https://www.edutubebd.com/file_resource/2091288073life_is_what_you_make_it.pdf"
+    });
+  };
 
-    render() {
-        return(
-            <View style={{flex: 1}}>
-            <WebView  source={{uri: "https://www.edutubebd.com/file_resource/2091288073life_is_what_you_make_it.pdf"}} />
-            <Button title={"Share"} onPress={() => this.share()}/>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <WebView
+          source={{
+            uri:
+              "http://docs.google.com/gview?embedded=true&url=https://www.edutubebd.com/file_resource/2091288073life_is_what_you_make_it.pdf"
+          }}
+        />
+        <Button title={"Share"} onPress={() => this.share()} />
+      </View>
+    );
+  }
 }
